2f49f023e5ea0673a58d22312f702028bef4e615
MalTok <mig01@wp.pl>
2023-09-04 11:30:48 +0200

    Rozwiązanie zadania

--- src/main/java/pl/javastart/task/Season.java
+++ src/main/java/pl/javastart/task/Season.java
@@ -1,5 +1,45 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
+
 public enum Season {
     //uzupełnij
+    SPRING("Wiosna", new String[]{"marzec", "kwiecień", "maj"}),
+    SUMMER("Lato", new String[]{"czerwiec", "lipiec", "sierpień"}),
+    AUTUMN("Jesień", new String[]{"wrzesień", "październik", "listopad"}),
+    WINTER("Zima", new String[]{"grudzień", "styczeń", "luty"});
+
+    private final String plName;
+    private final String[] months;
+
+    Season(String plName, String[] months) {
+        this.plName = plName;
+        this.months = months;
+    }
+
+    public String getPlName() {
+        return plName;
+    }
+
+    public String[] getMonths() {
+        return months;
+    }
+
+    @Override
+    public String toString() {
+        return plName + " - " + Arrays.toString(months);
+    }
+
+    public Season findSeasonByMonth(String month) {
+        Season foundSeason = null;
+        for (Season season : Season.values()) {
+            for (String value : season.months) {
+                if (value.equalsIgnoreCase(month)) {
+                    foundSeason = season;
+                    break;
+                }
+            }
+        }
+        return foundSeason;
+    }
 }
--- src/main/java/pl/javastart/task/SeasonManager.java
+++ src/main/java/pl/javastart/task/SeasonManager.java
@@ -1,12 +1,42 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SeasonManager {
 
     // uzupełnij metodę - do wczytywania danych użyj przekazany Scanner
     public void run(Scanner scanner) {
+        Season foundSeason = getSeasonFromUser(scanner);
+        showMonthsInSeason(foundSeason);
+    }
 
+    private Season getSeasonFromUser(Scanner scanner) {
+        Season foundSeason = null;
+        do {
+            System.out.println("Podaj porę roku:");
+            showSeasons();
+            String userInput = scanner.nextLine();
+            for (Season season : Season.values()) {
+                if (userInput.equalsIgnoreCase(season.getPlName())) {
+                    foundSeason = season;
+                    break;
+                }
+            }
+        } while (foundSeason == null);
+        return foundSeason;
+    }
+
+    private void showMonthsInSeason(Season season) {
+        System.out.println("W tej porze roku są następujące miesiące:");
+        String[] months = season.getMonths();
+        System.out.println(Arrays.toString(months));
+    }
+
+    private void showSeasons() {
+        for (Season season : Season.values()) {
+            System.out.println(season.getPlName());
+        }
     }
 
 }
